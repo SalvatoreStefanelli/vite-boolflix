da188b28b63d9f16567becfff7d1e2523b29a9ec
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-03-19 20:10:28 +0100

    add step2 with the filter of the series tv

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,18 @@
 <script>
 
 import axios from 'axios';
+import { store } from '../store.js';
+
 
 export default {
     name: 'AppMain',
     data() {
         return {
+            store,
             base_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=32b11fa0e734b79e146cf88e806df1e4&language=it-IT&query=',
+            base_api_url_tv: 'https://api.themoviedb.org/3/search/tv?api_key=32b11fa0e734b79e146cf88e806df1e4&language=it-IT&query=',
             movies: [],
+            series: [],
             error: false,
             searchText: ''
         }
@@ -30,21 +35,39 @@ export default {
                 })
         },
 
+        getSeries(url_tv) {
+            axios
+                .get(url_tv)
+                .then((response) => {
+                    console.log(response.data);
+                    this.series = response.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                    this.error = error.message;
+                })
+        },
+
         filterResults() {
             const url = `${this.base_api_url}${this.searchText}`
+            const url_tv = `${this.base_api_url_tv}${this.searchText}`
             console.log(url);
+            console.log(url_tv);
 
             this.getMovies(url);
+            this.getSeries(url_tv);
         }
     },
 
     computed: {
         getResults() {
             return this.movies.results ? this.movies.results.length : 'no results yet'
+            return this.series.results ? this.series.results.length : 'no results yet'
         }
     },
     created() {
         this.getMovies(this.base_api_url)
+        this.getSeries(this.base_api_url_tv)
     }
 }
 </script>
@@ -53,7 +76,7 @@ export default {
     <main>
 
         <div class="filters">
-            <input type="text" placeholder="Type a movie to search" v-model="searchText">
+            <input type="text" placeholder="Type a movie or a series TV to search" v-model="searchText">
             <button @click="filterResults">Search</button>
         </div>
 
@@ -63,6 +86,15 @@ export default {
             {{ movie.original_language }},
             {{ movie.vote_average }}
         </div>
+
+        <div v-for="serie in series.results" :key="serie.id + '_serie'">
+            {{ serie.name }},
+            {{ serie.original_name }},
+            {{ serie.original_language }},
+            {{ serie.vote_average }}
+        </div>
+
+
 
 
         <div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    base_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=32b11fa0e734b79e146cf88e806df1e4&query=ritorno+al+futuro&language=it-IT'
+    base_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=32b11fa0e734b79e146cf88e806df1e4&language=it-IT&query=',
+    base_api_url_tv: 'https://api.themoviedb.org/3/search/tv?api_key=32b11fa0e734b79e146cf88e806df1e4&language=it-IT&query='
 });
